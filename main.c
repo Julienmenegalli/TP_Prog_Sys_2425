5598d675f254cd6c49043dc6a52efb7cb469718e
Julienmenegalli <menegallijulien@gmail.com>
2024-11-27 16:23:35 +0100

    Q1-3

--- main.c
+++ main.c
@@ -8,16 +8,16 @@
 
 #define Welcome "$ ./enseash \nBienvenue dans le Shell enseash. \nTapez 'exit' pour quitter.\n"
 #define BUFFER_SIZE 128
-#define MaxCharCountenter 74
-#define MaxCharCountexit 13
 #define enseash "enseash % "
+#define end "Au revoir !\n"
+#define ReturnFortune "14 25 39 8 17\n"
 
 int main() {
     char input[BUFFER_SIZE];
     char prompt[] = enseash;
 
     // Message d'accueil
-    write(STDOUT_FILENO,Welcome , MaxCharCountenter);
+    write(STDOUT_FILENO,Welcome , strlen (Welcome));
 
     while (1) {
         // Affichage du prompt
@@ -31,9 +31,12 @@ int main() {
 
         // Si l'utilisateur tape "exit", sortir de la boucle
         if (strcmp(input, "exit") == 0) {
-            write(STDOUT_FILENO, "Au revoir !\n", MaxCharCountexit);
+            write(STDOUT_FILENO,end , strlen (end));
             break;
-            // Un commentaire très intéressant.
+        }
+        // Si l'utilisateur tape "fortune", lance la fonction fortune
+        if (strcmp(input, "fortune") == 0) {
+            write(STDOUT_FILENO,ReturnFortune, strlen(ReturnFortune));
         }
     }
     return 0;
