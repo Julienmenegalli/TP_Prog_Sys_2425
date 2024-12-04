bc25e1138e31de62a1e21e30e960bbeb167c27a3
JulienMenegalli <menegallijulien@gmail.com>
2024-12-04 17:38:24 +0100

    fin séance 2

--- main.c
+++ main.c
@@ -12,12 +12,13 @@
 #define end "Au revoir !\n"
 #define MSG_EXIT "[exit:%d "
 #define MSG_SIGN "[sign:%d "
-#define MSG_TIME " | %d ms] "
+#define MSG_TIME "| %d ms] "
 
 int main() {
     int status;
-
+    struct timespec start_time;
     char input[BUFFER_SIZE];
+    struct timespec end_time;
 
     // first message
     write(STDOUT_FILENO,Welcome , strlen (Welcome));
@@ -30,6 +31,8 @@ int main() {
         // read user entry
         ssize_t bytes_read = read(STDIN_FILENO, input, BUFFER_SIZE - 1);
 
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+
         //  add a terminator null, to make a valid chain
         input[bytes_read - 1] = '\0'; // Remplace '\n' par '\0'
 
@@ -38,6 +41,7 @@ int main() {
             write(STDOUT_FILENO,end , strlen (end));
             exit(EXIT_SUCCESS);
         }
+
         if (fork() == 0) {
             execlp(input,input, NULL);
             exit(EXIT_FAILURE);
@@ -53,9 +57,16 @@ int main() {
             char message_to_sent[BUFFER_SIZE] = {'\0'};
             snprintf(message_to_sent, 128, MSG_SIGN, WTERMSIG(status));
             write(STDOUT_FILENO, message_to_sent, strlen(message_to_sent));
-        }
-    }
 
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        int time =  (end_time.tv_sec-start_time.tv_sec) * 1e3 +  (end_time.tv_nsec-start_time.tv_nsec) /
+        1e6;
+        char time_to_sent[BUFFER_SIZE] = {'\0'};
+        snprintf(time_to_sent,128,MSG_TIME,time);
+        write(STDOUT_FILENO, time_to_sent, strlen(time_to_sent));
+    }
     return 0;
 }
 
