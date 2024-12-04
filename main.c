3ae126f0846de0c58f05469f5aa126b73b5bc56c
JulienMenegalli <menegallijulien@gmail.com>
2024-12-04 16:34:22 +0100

    Q4

--- main.c
+++ main.c
@@ -3,24 +3,29 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #define Welcome "$ ./enseash \nBienvenue dans le Shell enseash. \nTapez 'exit' pour quitter.\n"
 #define BUFFER_SIZE 128
-#define enseash "enseash % "
+#define enseash "enseash"
+#define pourcentage " % "
 #define end "Au revoir !\n"
-#define MSG_EXIT "[exit:%d"
-#define MSG_SIGN "[sign:%d"
+#define MSG_EXIT "[exit:%d "
+#define MSG_SIGN "[sign:%d "
+#define MSG_TIME " | %d ms] "
 
 int main() {
+    int status;
+
     char input[BUFFER_SIZE];
-    char prompt[] = enseash;
 
     // first message
     write(STDOUT_FILENO,Welcome , strlen (Welcome));
 
     while (1) {
         // print prompt
-        write(STDOUT_FILENO, prompt, strlen(prompt));
+        write(STDOUT_FILENO, enseash, strlen(enseash));
+        write(STDOUT_FILENO,pourcentage,strlen(pourcentage));
 
         // read user entry
         ssize_t bytes_read = read(STDIN_FILENO, input, BUFFER_SIZE - 1);
@@ -35,23 +40,22 @@ int main() {
         }
         if (fork() == 0) {
             execlp(input,input, NULL);
-            exit(EXIT_SUCCESS);
+            exit(EXIT_FAILURE);
         }
-        void write_message(char *message, int val){
+        else {wait(&status);}
+
+        if (WIFEXITED(status)){
             char message_to_sent[BUFFER_SIZE] = {'\0'};
-            snprintf(message_to_sent, strlen(message_to_sent), message, val);
+            snprintf(message_to_sent, 128, MSG_EXIT, WEXITSTATUS(status));
             write(STDOUT_FILENO, message_to_sent, strlen(message_to_sent));
         }
-
-        void print_status(int status) {
-            if (WIFEXITED(status)) {
-                write_message(MSG_EXIT, WEXITSTATUS(status));
-            }
-            else if (WIFSIGNALED(status)) {
-                write_message(MSG_SIGN, WTERMSIG(status));
-            }
+        else if (WIFSIGNALED(status)) {
+            char message_to_sent[BUFFER_SIZE] = {'\0'};
+            snprintf(message_to_sent, 128, MSG_SIGN, WTERMSIG(status));
+            write(STDOUT_FILENO, message_to_sent, strlen(message_to_sent));
         }
     }
+
     return 0;
 }
 
